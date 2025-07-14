23e57cd69f44384167b4094a2b9f0a6898ec5c1d
pinkyagrawal <129983773+pinkyagrawal@users.noreply.github.com>
2025-07-14 23:10:09 +0530

    Delete core java directory

--- core java/Day-02_ControlFlow/SmallestOfThree.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class SmallestOfThree {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt(), num2 = sc.nextInt(), num3 = sc.nextInt();
-        boolean isSmallest = num1 < num2 && num1 < num3;
-        System.out.println("Is the first number the smallest? " + isSmallest);
-    }
-}
